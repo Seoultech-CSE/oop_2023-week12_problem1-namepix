1e0b318592e3474461731a634a68cb191959455a
namepix <132027955+namepix@users.noreply.github.com>
2023-05-26 10:30:10 +0900

    Update Problem1.java
    Arraylist<Number> 를 활용하여 두가지 타입의 요소들을 한 list객체에 집어놓고 정렬하여 각 타입에 맞게 출력하도록 하였다.

--- Problem1.java
+++ Problem1.java
@@ -1,29 +1,52 @@
 import java.util.*;
 
-public class week13_1 {
+public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         // 필요한 자료형을 넣어주세요
-        ArrayList< __________ > list = new ArrayList< __________ >();
+        ArrayList<Number> list = new ArrayList<Number>();
         System.out.print("Enter five integers and five doubles: ");
 
         /*
             list 입력 코드
          */
+        System.out.println("Enter five integers and five doubles: ");
+        for(int i=0; i<5; i++){
+            int a = input.nextInt();
+            list.add(a);
+        }
+        for(int i=0; i<5; i++){
+            double a = input.nextDouble();
+            list.add(a);
+        }
+
 
         sort(list);
 
         /*
             list 출력 코드
          */
+        for(Number num : list){
+            if (num instanceof Integer) {
+                System.out.print(num + " ");
+            }
+            else if (num instanceof Double) {
+                System.out.print(num + " ");
+            }
+        }
     }
 
-    public static void sort( __________ ) {
-
+    public static void sort(ArrayList<Number> list) {
          /*
             list 정렬 코드
          */
-
+        Collections.sort(list, new java.util.Comparator<Number>() {
+            public int compare(Number num1, Number num2) {
+                return Double.compare(num1.doubleValue(), num2.doubleValue());
+            }
+        }
+        );
     }
 }
+
